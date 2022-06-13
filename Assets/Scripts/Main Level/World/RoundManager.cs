72fc11685dbefb8b746b535d7ae54902dd1f86ab
MichaelShular <60903307+MichaelShular@users.noreply.github.com>
2022-06-13 17:21:01 -0400

    Round manager in-progress

--- Assets/Scripts/Main Level/World/RoundManager.cs
+++ Assets/Scripts/Main Level/World/RoundManager.cs
@@ -1,32 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+using Unity.Netcode;
 public enum RoundType
 {
     PossessionRound,
     SeekingRound,
     EndGame
 }
-public class RoundManager : MonoBehaviour
+public class RoundManager : NetworkBehaviour
 {
+    public static event Action StartRoundEvent;
+    public NetworkVariable<bool> m_changeRound = new NetworkVariable<bool>();
+
+
     [Header("Round Settings")]
     [SerializeField]
     private float m_possessionRoundAmountOfTime;
     private RoundType m_currentRound;
+    
 
+
+
+    private void Awake()
+    {
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(StartPossessionRound());
+        m_changeRound.Value = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(m_changeRound.Value);
+        if (m_changeRound.Value)
+        {
+            StartRoundEvent?.Invoke();
+            m_changeRound.Value = false;
+        }
     }
+
+    public void StartGame()
+    {
+        StartCoroutine(StartPossessionRound());
+        Debug.Log("Game Started");
+    }
+
 
     IEnumerator StartPossessionRound()
     {
@@ -34,8 +57,14 @@ public class RoundManager : MonoBehaviour
         yield return new WaitForSeconds(m_possessionRoundAmountOfTime);
         Debug.Log("Start Seek Round");
         m_currentRound = RoundType.SeekingRound;
+        StartRoundEvent?.Invoke();
+        SubmitEventRequestServerRpc(true);
     }
 
-
+    [ServerRpc]
+    void SubmitEventRequestServerRpc(bool a)
+    {
+        m_changeRound.Value = a;
+    }
 
 }
